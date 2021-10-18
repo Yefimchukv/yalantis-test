4ced0e876a420a2ff80513387941e7cd57b7464e
Vitaliy Yefimchuk <fry2000.3000@gmail.com>
2021-10-18 15:39:04 +0300

    first commit

---
+++ YalantisTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YalantisTest
+//
+//  Created by Vitaliy Yefimchuk on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
